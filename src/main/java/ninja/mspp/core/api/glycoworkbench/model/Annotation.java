15dcffac1f271ef8c1a0d6d751e093c5aeadfecf
Satoshi Tanaka <satstnka@gmail.com>
2025-03-01 16:37:58 +0900

    added Annotation class in GlycoWorkbench service.

---
+++ src/main/java/ninja/mspp/core/api/glycoworkbench/model/Annotation.java
@@ -0,0 +1,36 @@
+package ninja.mspp.core.api.glycoworkbench.model;
+
+public class Annotation {
+	private double mass;
+	private double intensity;
+	private String image;
+	
+	public Annotation() {
+		this.mass = 0.0;
+		this.image = "";
+	}
+
+	public double getMass() {
+		return mass;
+	}
+
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+
+	public double getIntensity() {
+		return intensity;
+	}
+
+	public void setIntensity(double intensity) {
+		this.intensity = intensity;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+}
